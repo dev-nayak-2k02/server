a6e0ededfd08c521f518afa78a90546cdea9cc73
dev nayak10 <devprasannanayak@gmail.com>
2025-10-16 18:12:38 +0530

    content added

---
+++ Operations/151025.js
@@ -0,0 +1,14 @@
+// let num = 10;
+// while(num!=0){
+//     console.log(num);
+//     num = num-1;
+// }
+
+let num = 3;
+let ctr = 1;
+let mul = 0;
+while(mul<30){
+    mul = num * ctr;
+    console.log(mul);
+    ctr++;
+}
