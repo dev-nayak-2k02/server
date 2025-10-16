9a09114a0578438fe65ff24d5806ac5e8f59dbfc
dev nayak10 <devprasannanayak@gmail.com>
2025-10-16 18:20:09 +0530

    content added

--- Operations/161025.js
+++ Operations/161025.js
@@ -1,2 +1,13 @@
-let str = 'apple';
-console.log(str.split('p'));
+// let str = 'apple';
+// console.log(str.split('p'));
+
+
+// let str = 'apple';
+// console.log(str.replaceAll('p','i'));
+
+
+// let str = 'DevPrasanna';
+// console.log(str.indexOf('P'))
+
+let str = 'Irokuni';
+console.log(str.includes('f'));
