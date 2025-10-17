045f836e6474f10adbdd055d53d7e06577a7e5e3
dev nayak10 <devprasannanayak@gmail.com>
2025-10-17 19:43:18 +0530

    content added

--- Operations/161025.js
+++ Operations/161025.js
@@ -1,27 +1,40 @@
-let ari = 'apple';
-console.log(ari.split('p'));
+// let ari = 'apple';
+// console.log(ari.split('p'));
 
 
-let trs = 'apple';
-console.log(trs.replaceAll('p','i'));
+// let trs = 'apple';
+// console.log(trs.replaceAll('p','i'));
 
 
-let der = 'DevPrasanna';
-console.log(der.indexOf('P'));
+// let der = 'DevPrasanna';
+// console.log(der.indexOf('P'));
 
-let str = 'Irokuni';
-console.log(str.includes('f'));
+// let str = 'Irokuni';
+// console.log(str.includes('f'));
 
-let ans = (function(){
-    let a;
-    return {
-        set: function(val){
-            a = val;
-        },
-        get: function(){
-            console.log(a);
-        }
-    }
-})();
-ans.set(10)
-ans.get()
+// let ans = (function(){
+//     let a;
+//     return {
+//         set: function(val){
+//             a = val;
+//         },
+//         get: function(){
+//             console.log(a);
+//         }
+//     }
+// })();
+// ans.set(10)
+// ans.get()
+
+// for(let i = 1; i<=4; i++){
+//     let star="";
+//     for(let j = 1; j<=i; j++){
+//         star+='*';
+//     }
+//     console.log(star);
+// }
+
+let str = 'javaScript';
+for(let eng of str){
+    console.log(eng + eng);
+}
