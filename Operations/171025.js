78243bcad5f21e37e613fa80c4594c61063e9222
dev nayak10 <devprasannanayak@gmail.com>
2025-10-19 11:10:45 +0530

    content added

--- Operations/171025.js
+++ Operations/171025.js
@@ -1,4 +1,7 @@
-let arr = [3, 1, 4, 3, 1, 4, 2, 5];
-let arri = [...new Set(arr)];   // to find unique values
-let res = arri.sort();
-console.log(res[1]);
+// let arr = [3, 1, 4, 3, 1, 4, 2, 5];
+// let arri = [...new Set(arr)];   // to find unique values
+// let res = arri.sort();
+// console.log(res[1]);
+
+
+
---
+++ Operations/181025.js
@@ -0,0 +1,31 @@
+// var a = 1;
+// var i = 1;
+// while(i<=10){
+//     var sqr = a * i;
+//     console.log(sqr);
+//     a++;
+//     i++;
+// }
+
+
+// let num = Number(prompt('enter your number... '));
+// let ctr = 0;
+//num === 1 break; console.log('not prime');
+// for(let i=1; i<=num; i++){
+//     if (num%i == 0) {
+//         ctr++;
+//     }
+// }
+// if(ctr == 2) {
+//     console.log('it is prime');
+// } else {
+//     console.log('it is not prime');
+// }
+
+let a = Number(prompt("enter a number"));
+let b = Number(prompt("enter a number"));
+console.log(a**b);  
+    
+// } else {
+//     console.log('power is not raised to base');
+// }
