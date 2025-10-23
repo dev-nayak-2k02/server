c1e8bb5153aa6e3e5d00caa41956b5b5d3f93731
dev nayak10 <devprasannanayak@gmail.com>
2025-10-23 08:25:35 +0530

    content added

---
+++ Operations/231025.js
@@ -0,0 +1,19 @@
+function register(){
+    console.log("Register called");
+}
+
+function welcome(){
+    console.log('Welcome to our application');
+}
+
+function login(){
+    console.log('login Here');
+}
+
+function fetchData(){
+    console.log('data fetched');
+}
+
+function displayData(){
+    console.log('data is displayed');
+}
