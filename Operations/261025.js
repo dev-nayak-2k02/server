85ba9101d92c239655b7ea56bff2af6417aa9145
dev nayak10 <devprasannanayak@gmail.com>
2025-10-26 22:52:19 +0530

    content added

--- Operations/261025.js
+++ Operations/261025.js
@@ -1,49 +1,61 @@
 // console.log([] == ![]);
 
 
-function register(callBack){
-    setTimeout(()=>{
-        console.log('register page');
-        callBack();
-    },1000);
-}
+// function register(callBack){
+//     setTimeout(()=>{
+//         console.log('register page');
+//         callBack();
+//     },1000);
+// }
 
-function welcome (cb){
-    setTimeout(()=>{
-        console.log('Welcome! aboard');
-        cb();
-    },1000);
-}
+// function welcome (cb){
+//     setTimeout(()=>{
+//         console.log('Welcome! aboard');
+//         cb();
+//     },1000);
+// }
 
-function login(dr){
-    setTimeout(()=>{
-        console.log('Login Page');
-        dr();
-    },1000);
-}
+// function login(dr){
+//     setTimeout(()=>{
+//         console.log('Login Page');
+//         dr();
+//     },1000);
+// }
 
 
-function fetchData(fn){
-    setTimeout(()=>{
-        console.log('Data is fetched');
-        fn();
-    },1000);
-}
+// function fetchData(fn){
+//     setTimeout(()=>{
+//         console.log('Data is fetched');
+//         fn();
+//     },1000);
+// }
 
-function displayData(){
-    setTimeout(()=>{
-        console.log('Data is being displayed');
-    },1000);
-}
+// function displayData(){
+//     setTimeout(()=>{
+//         console.log('Data is being displayed');
+//     },1000);
+// }
 
-register(()=>{
-    welcome(()=>{
-        login(()=>{
-            fetchData(()=>{
-                displayData()
-            })
-        })
-    })
-});
+// register(()=>{
+//     welcome(()=>{
+//         login(()=>{
+//             fetchData(()=>{
+//                 displayData()
+//             })
+//         })
+//     })
+// });
+
+
+
+let result = false;
+let prom = new Promise((resolve, reject) => {
+    if (result) {
+        resolve();
+    } else {
+        reject();
+    }
+})
+console.log(prom);
 
 
