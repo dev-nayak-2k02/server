3cbbc78e0cf0f461351e215f42aecd3919ebf9f5
dev nayak10 <devprasannanayak@gmail.com>
2025-10-26 22:45:34 +0530

    content added

--- Operations/261025.js
+++ Operations/261025.js
@@ -1,1 +1,49 @@
-console.log([] == ![]);
+// console.log([] == ![]);
+
+
+function register(callBack){
+    setTimeout(()=>{
+        console.log('register page');
+        callBack();
+    },1000);
+}
+
+function welcome (cb){
+    setTimeout(()=>{
+        console.log('Welcome! aboard');
+        cb();
+    },1000);
+}
+
+function login(dr){
+    setTimeout(()=>{
+        console.log('Login Page');
+        dr();
+    },1000);
+}
+
+
+function fetchData(fn){
+    setTimeout(()=>{
+        console.log('Data is fetched');
+        fn();
+    },1000);
+}
+
+function displayData(){
+    setTimeout(()=>{
+        console.log('Data is being displayed');
+    },1000);
+}
+
+register(()=>{
+    welcome(()=>{
+        login(()=>{
+            fetchData(()=>{
+                displayData()
+            })
+        })
+    })
+});
+
+
