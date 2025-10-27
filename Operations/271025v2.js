0aafdac2f5a0cc99c15eded9562264af67087fe0
dev nayak10 <devprasannanayak@gmail.com>
2025-10-27 09:50:13 +0530

    content added

--- Operations/271025v2.js
+++ Operations/271025v2.js
@@ -1,13 +1,27 @@
-function register (arg){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if(arg){
-                resolve('resolved')
-            } else {
-                reject('rejected')
-            }
-        }, 1000);
-    })
+function register(arg) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (arg) {
+        resolve("resolved");
+      } else {
+        reject("rejected");
+      }
+    }, 1000);
+  });
 }
 
-register(true).then((res)=>console.log(res));
+function welcome(cb) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (cb) {
+        resolve("resolved");
+      } else {
+        reject("rejected");
+      }
+    }, 1000);
+  });
+}
+
+register(true)
+  .then((res) => {console.log(res); return welcome(true);})
+  .then((res) => console.log(res));
