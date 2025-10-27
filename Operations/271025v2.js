301605e689139a3db4b43bd435e47813f00f9175
dev nayak10 <devprasannanayak@gmail.com>
2025-10-27 09:53:51 +0530

    content added

--- Operations/271025v2.js
+++ Operations/271025v2.js
@@ -22,6 +22,32 @@ function welcome(cb) {
   });
 }
 
+function login(cb) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (cb) {
+        resolve("resolved");
+      } else {
+        reject("rejected");
+      }
+    }, 1000);
+  });
+}
+
+function fetchData(cb) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (cb) {
+        resolve("resolved");
+      } else {
+        reject("rejected");
+      }
+    }, 1000);
+  });
+}
+
 register(true)
   .then((res) => {console.log(res); return welcome(true);})
-  .then((res) => console.log(res));
+  .then((res) => {console.log(res); return login(true);})
+  .then((res) => {console.log(res); return fetchData(true);})
+  .then((res) => {console.log(res);})
