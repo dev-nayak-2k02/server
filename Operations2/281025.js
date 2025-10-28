9b732ceeee6d8978f0ea141561ed206c7c62fa2c
dev nayak10 <devprasannanayak@gmail.com>
2025-10-28 18:52:00 +0530

    content added

--- Operations2/281025.js
+++ Operations2/281025.js
@@ -1,5 +1,14 @@
-let arr = [1,2,3,4,5,6,7,8,9,10,11]
-let ans = arr.reduce((accumulator, initialValue) =>{
-    return accumulator += initialValue;
-},1)
-console.log(ans);
+// let arr = [1,2,3,4,5,6,7,8,9,10,11]
+// let ans = arr.reduce((accumulator, initialValue) =>{
+//     return accumulator += initialValue;
+// },1)
+// console.log(ans);
+
+
+for (let i = 1; i <= 5; i++) {
+    let star = '';
+    for(let j = 1; j <= i; j++){
+        star += '*';
+    }
+    console.log(star);
+}
