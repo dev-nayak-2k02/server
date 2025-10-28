65d1b6fb17465c6625e0ecdd9d76c9061df9045a
dev nayak10 <devprasannanayak@gmail.com>
2025-10-28 12:29:04 +0530

    content added

--- Operations2/281025.js
+++ Operations2/281025.js
@@ -1,3 +1,5 @@
-let arr = [1,2,3,4,5,6,7,8]
-let ans = arr.filter(item => item%3 == 0);
+let arr = [1,2,3,4,5,6,7,8,9,10,11]
+let ans = arr.reduce((accumulator, initialValue) =>{
+    return accumulator += initialValue;
+},1)
 console.log(ans);
