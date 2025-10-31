aa7019a8482a5595ce2d3640ded2615ac2c1157d
dev nayak10 <devprasannanayak@gmail.com>
2025-10-31 08:33:35 +0530

    content added

--- Operations/311025.js
+++ Operations/311025.js
@@ -1,5 +1,5 @@
 const now = new Date();
-const day = now.getDay().toLocaleString();;
+const day = now.getDay();
 const date = now.getDate()
 const hours = now.getHours();
 const min = now.getMinutes();
@@ -8,5 +8,5 @@ const sec = now.getSeconds();
 const days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
 
 console.log(`Todays Date: ${date}`);
-console.log(`Todays Day: ${day}`);
+console.log(`Todays Day: ${days[day-1]}`);
 console.log(`Time: ${hours}:${min}:${sec}`);
