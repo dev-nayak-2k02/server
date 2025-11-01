5d82f18a503565a222b5003134d5f0852f962eee
dev nayak10 <devprasannanayak@gmail.com>
2025-11-01 09:41:16 +0530

    content added

--- Operations/011125.js
+++ Operations/011125.js
@@ -1,8 +1,18 @@
-let a = 7;
-let fact = 0;
-for(let i = 1; i <= a; i++){
-    if(a%i == 0){
-        fact++;
-    }
+// let a = 7;
+// let fact = 0;
+// for(let i = 1; i <= a; i++){
+//     if(a%i == 0){
+//         fact++;
+//     }
+// }
+// console.log(fact);
+
+
+
+let n = 22;
+let count = 0;
+while(n!=0) {
+    count++;
+    n = parseInt(n/10);
 }
-console.log(fact);
+console.log(count);
