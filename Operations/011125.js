704d8bc470350fce13074d40f1bfa18674098357
dev nayak10 <devprasannanayak@gmail.com>
2025-11-01 20:46:32 +0530

    content added

--- Operations/011125.js
+++ Operations/011125.js
@@ -9,10 +9,22 @@
 
 
 
-let n = 22;
-let count = 0;
-while(n!=0) {
-    count++;
-    n = parseInt(n/10);
-}
-console.log(count);
+// let n = 22;
+// let count = 0;
+// while(n!=0) {
+//     count++;
+//     n = parseInt(n/10);
+// }
+// console.log(count);
+
+
+// function higherOrderFunction(fnc, time, clr){
+//     setInterval(fnc, time)
+//     setTimeout(() => {
+//         clearInterval()
+//     }, clr);
+// }
+
+// higherOrderFunction(function callback(){
+//     console.log('Hello, world!');
+// }, 2000, 6000)
