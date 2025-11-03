e33bbf435618be3d8b83259002d1ea284fa70b90
dev nayak10 <devprasannanayak@gmail.com>
2025-11-03 15:25:28 +0530

    content added

--- Operations/031125.js
+++ Operations/031125.js
@@ -1,19 +1,52 @@
-checkPrime(prime,2)
+// checkPrime(prime,2)
 
-function checkPrime(fnc,a){
-    fnc(a);
+// function checkPrime(fnc,a){
+//     fnc(a);
+// }
+
+// function prime(a){
+//     if(a<=1){
+//         console.log('not-prime');
+//         return 
+//     }
+//     let ctr = 0;
+//     for(let i = 1; i<=a; i++){
+//         if(a%i == 0){
+//             ctr++;
+//         }
+//     }
+//     ctr === 2? console.log('Prime') : console.log('not-Prime');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function register(fnc){
+    setTimeout(()=>{
+        console.log('regster done');
+        fnc()
+    })
 }
 
-function prime(a){
-    if(a<=1){
-        console.log('not-prime');
-        return 
-    }
-    let ctr = 0;
-    for(let i = 1; i<=a; i++){
-        if(a%i == 0){
-            ctr++;
-        }
-    }
-    ctr === 2? console.log('Prime') : console.log('not-Prime');
+function welcome(fnc){
+    setTimeout(()=>{
+        console.log('welcome done');
+        fnc()
+    })
 }
+
+register(()=>{
+    welcome()
+})
