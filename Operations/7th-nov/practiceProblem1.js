76287b1b05261211393c55c06c124c8dec4b1f90
dev nayak10 <devprasannanayak@gmail.com>
2025-11-07 19:49:01 +0530

    content added

--- Operations/7th-nov/practiceProblem1.js
+++ Operations/7th-nov/practiceProblem1.js
@@ -6,7 +6,23 @@
 // console.log(conCurrent);
 
 //second largest
-let arr = [1,2,3,4,5];
-let arri = [...new Set(arr)];
-let res = arri.sort((a, b)=> b-a);
-console.log(res[1]);
+// let arr = [1,2,3,4,5];
+// let arri = [...new Set(arr)];
+// let res = arri.sort((a, b)=> b-a);
+// console.log(res[1]);
+
+
+//most frequrntly occured element
+let arr = [1,2,3,3,2,4,8,3];
+let obj = new Object();
+arr.forEach((val)=>{
+    obj[val] === undefined ? (obj[val] = 1) : (obj[val]++)
+})
+let ctr = 0;
+for(let data in obj){
+    if(ctr<obj[data]){
+        ctr = obj[data];
+    }
+}
+
+console.log(`number with highest occurence frequency is ${ctr}`);
