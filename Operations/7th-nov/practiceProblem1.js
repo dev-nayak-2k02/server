53f87a698caee7e20796a5eb44fd403422cfff41
dev nayak10 <devprasannanayak@gmail.com>
2025-11-07 19:38:07 +0530

    content added

--- Operations/7th-nov/practiceProblem1.js
+++ Operations/7th-nov/practiceProblem1.js
@@ -4,3 +4,9 @@
 //     b.has(value)
 // }))
 // console.log(conCurrent);
+
+//second largest
+let arr = [1,2,3,4,5];
+let arri = [...new Set(arr)];
+let res = arri.sort((a, b)=> b-a);
+console.log(res[1]);
