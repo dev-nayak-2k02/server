c98b3b67e079c65a087aa83957a2ed08b177a482
dev nayak10 <devprasannanayak@gmail.com>
2025-11-08 17:47:45 +0530

    content added

---
+++ Operations/8th-nov/practiceProblem2.js
@@ -0,0 +1,18 @@
+function throttle(cb, delay) {
+  let lastCall = 0;
+  return function () {
+    let current = Date.now();
+    if (current - lastCall >= delay) {
+      lastCall = current;
+      cb();
+    }
+  };
+}
+
+let newFnc = throttle(() => {
+  let num = Math.floor(Math.random());
+  console.log(num);
+}, 2000);
+
+
+newFnc()
