bd2a8ed92ff27a76b3a75e7083c672de36762390
dev nayak10 <devprasannanayak@gmail.com>
2025-11-17 18:11:58 +0530

    content added

--- Operations/17th-Nov/practiceProblem1.js
+++ Operations/17th-Nov/practiceProblem1.js
@@ -1,2 +1,8 @@
-let str = 'Hello there';
-console.log(str.slice(2,5));
+//slice
+// let str = 'Hello there';
+// console.log(str.slice(2,5));
+
+
+//split
+let str = 'Hello there we re back';
+console.log(str.split('e'));
