772ec9e075e46a071e06284ecadc8ad0c604d91e
dev nayak10 <devprasannanayak@gmail.com>
2025-11-17 18:16:35 +0530

    content added

--- Operations/17th-Nov/practiceProblem1.js
+++ Operations/17th-Nov/practiceProblem1.js
@@ -4,5 +4,21 @@
 
 
 //split
-let str = 'Hello there we re back';
-console.log(str.split('e'));
+// let str = 'Hello there we re back';
+// console.log(str.split('e'));
+
+
+//replace
+// let str = 'hello';
+// console.log(str.replace('o','aw'));
+
+//repalceAll
+// let str = 'hellaw';
+// console.log(str.replaceAll('l','k'));
+
+
+//indexOf
+// let str = 'UltraBee';
+// console.log(str.indexOf('a'));
+
+//
