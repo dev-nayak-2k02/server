b334b8b1cbb40ceafde8ce49939ff05ac52dcb40
dev nayak10 <devprasannanayak@gmail.com>
2025-11-18 19:24:09 +0530

    content added

---
+++ Operations/18th-Nov/practiceProblem6.js
@@ -0,0 +1,25 @@
+// function parentFuntion(fn, n){
+//     setInterval(() => {
+//         fn();
+//     }, n);
+// }
+// parentFuntion(()=>{console.log('executed')}, 2000)
+
+
+// function mainFunction(fn) {
+//   let execute = false;
+//   return function () {
+//     if (!execute) {
+//       execute = true;
+//       fn();
+//     }
+//   };
+// }
+// let res = mainFunction(() => {
+//   console.log("executed!!");
+// });
+// res();
+// res();
+// res();
+
+
