54716e8577ee4da5d4d447c50b2252d5ed529999
dev nayak10 <devprasannanayak@gmail.com>
2025-11-18 18:54:34 +0530

    content added

--- Operations/18th-Nov/practiceProblem5.js
+++ Operations/18th-Nov/practiceProblem5.js
@@ -17,17 +17,32 @@
 
 
 //reverse an array without 
-let arr = [1,2,3,4];
-let a,b;
-a=0;
-b=arr.length-1;
-let ctr = 0;
-while(a<b){
-    ctr = arr[a];
-    arr[a] = arr[b];
-    arr[b] = ctr;
-    a++;
-    b--;
-}
+// let arr = [1,2,3,4];
+// let a,b;
+// a=0;
+// b=arr.length-1;
+// let ctr = 0;
+// while(a<b){
+//     ctr = arr[a];
+//     arr[a] = arr[b];
+//     arr[b] = ctr;
+//     a++;
+//     b--;
+// }
+// console.log(arr);
 
-console.log(arr);
+
+//most frequent number
+// let arr = [1,2,2,3,4,3,3,5,6,7,8,8];
+// let obj = {};
+// let ctr = 0
+// let num = 0;
+// arr.forEach((item)=>{
+//     obj[item] === undefined? (obj[item] = 1) : obj[item]++;
+// })
+// for(let key in obj){
+//     if(obj[key]>ctr){
+//         num=key;
+//     }
+// }
+// console.log(num);
