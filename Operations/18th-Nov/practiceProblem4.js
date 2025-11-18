42b5d45d2d5ca085e9ec8ddab945772965520c87
dev nayak10 <devprasannanayak@gmail.com>
2025-11-18 18:12:36 +0530

    content added

--- Operations/18th-Nov/practiceProblem4.js
+++ Operations/18th-Nov/practiceProblem4.js
@@ -1,5 +1,21 @@
-let arr = [1,2,3,4,5];
-let ans = arr.reduce((acc, val)=>{
-    return acc+val;
-},0)
-console.log(ans);
+// let arr = [1,2,3,4,5];
+// let ans = arr.reduce((acc, val)=>{
+//     return acc+val;
+// },0)
+// console.log(ans);
+
+// let a;
+// let b = null;
+// let c = a+b;
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+
+for(let i = 1; i<=5; i++){
+    let star = '';
+    for(let j = 1; j<=i; j++){
+        star += '*';
+    }
+    console.log(star);
+}
