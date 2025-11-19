c26611f96befde530ff20450f9b79b9862d8c753
dev nayak10 <devprasannanayak@gmail.com>
2025-11-19 12:09:37 +0530

    content added

--- Operations/19th-Nov/practiceProblem2.js
+++ Operations/19th-Nov/practiceProblem2.js
@@ -1,22 +1,37 @@
-let complete = false;
-let prom = new Promise((resolve, reject) => {
-  if (complete) {
-    resolve("resolved");
-  } else {
-    reject("fail");
-  }
-});
-console.log(prom);
+// let complete = false;
+// let prom = new Promise((resolve, reject) => {
+//   if (complete) {
+//     resolve("resolved");
+//   } else {
+//     reject("fail");
+//   }
+// });
+// console.log(prom);
+
+
+
+
+// function prom(complete){
+//     return new Promise((resolve, reject) => {
+//         if(complete){
+//             resolve('Resolved');
+//         } else {
+//             reject('Rejected')
+//         }
+//     })
+// }
+// const onFulfillment = (result)=>{
+//     console.log(result);
+// }
+// const onRejection = (error)=>{
+//     console.log(error);
+// }
+// prom(true).then(onFulfillment);
+// prom(false).then(onRejection);
 
 
 
 
 function prom(complete){
-    return new Promise((resolve, reject) => {
-        if(complete){
-            resolve('Resolved');
-        } else {
-            reject('Rejected')
-        }
-    })
+    
 }
