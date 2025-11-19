ec52e95ce63a45d9d68aa6534d53a2cbb381a2ab
dev nayak10 <devprasannanayak@gmail.com>
2025-11-19 16:01:08 +0530

    content added

--- DOM/lang.js
+++ DOM/lang.js
@@ -1,1 +1,8 @@
-let a = document
+document.addEventListener("DOMContentLoaded", () => {
+    let a = document.querySelector('#box');
+    let btn = document.querySelector('button');
+
+    btn.addEventListener('click', () => {
+        a.style.backgroundColor = 'red';
+    });
+});
