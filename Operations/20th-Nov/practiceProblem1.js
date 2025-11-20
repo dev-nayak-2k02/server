32e4143b52ea24c8248678c3d712eee60c36896b
dev nayak10 <devprasannanayak@gmail.com>
2025-11-20 15:36:07 +0530

    content added

--- Operations/20th-Nov/practiceProblem1.js
+++ Operations/20th-Nov/practiceProblem1.js
@@ -1,13 +1,28 @@
 //implement a fnc that accepts a callback and only executes it once
-function mainFunction(cb){
-    let executed = false;
-    return ()=>{
-        if(!executed){
-            executed = true;
-            cb();
+// function mainFunction(cb){
+//     let executed = false;
+//     return ()=>{
+//         if(!executed){
+//             executed = true;
+//             cb();
+//         }
+//     }
+// }
+// let ans = mainFunction(()=>{console.log('execution complete')})
+// ans();
+// ans();
+
+
+
+//Implement a fnc that throttles another fnc
+function throt(fn, delay){
+    let lastCall = 0;
+    return function (){
+        let current = Date.now();
+        if(current - lastCall >= delay){
+            lastCall = current;
+            fn();
         }
     }
 }
-let ans = mainFunction(()=>{console.log('execution complete')})
-ans();
-ans();
+let ans = throt(()=>{console.log('execution happened')},2000);
