ebd1703f7405c4fa49d4236a5bc509be5713d66e
dev nayak10 <devprasannanayak@gmail.com>
2025-11-23 22:33:04 +0530

    content added

--- Operations/23rd-Nov/practiceProblem.js
+++ Operations/23rd-Nov/practiceProblem.js
@@ -1,17 +1,23 @@
-function checkPerfect(n){
-    let num = n;
-    let count = 0;
-    for(let i=1;i<num;i++){
-        if(num%i == 0){
-            count+=i;
-        }
-    }
-    if(count == n){
-        return 'true';
-    } else {
-        return 'false';
-    }
-}
+// function checkPerfect(n){
+//     let num = n;
+//     let count = 0;
+//     for(let i=1;i<num;i++){
+//         if(num%i == 0){
+//             count+=i;
+//         }
+//     }
+//     if(count == n){
+//         return 'true';
+//     } else {
+//         return 'false';
+//     }
+// }
 
-let ans = checkPerfect(6)
-console.log(ans)
+// let ans = checkPerfect(6)
+// console.log(ans)
+
+
+
+// let arr = [1,2,3,4,5,6]
+// let arr2 = arr.sort((a, b)=>b-a)
+// console.log(arr2);
