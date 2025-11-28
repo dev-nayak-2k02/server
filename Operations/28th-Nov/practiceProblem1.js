5e18895570b0457dcd3a5fe706fa677b0d492f5c
dev nayak10 <devprasannanayak@gmail.com>
2025-11-28 22:23:31 +0530

    content added

--- Operations/28th-Nov/practiceProblem1.js
+++ Operations/28th-Nov/practiceProblem1.js
@@ -1,13 +1,20 @@
-function runOnce(fn){
-    let executed = false;
-    return function(){
-        if(!executed){
-            executed = true;
-            fn();
-        }
-    }
-}
-let ans = runOnce(()=>{
-    console.log('hello world');
-})
+// function runOnce(fn){
+//     let executed = false;
+//     return function(){
+//         if(!executed){
+//             executed = true;
+//             fn();
+//         }
+//     }
+// }
+// let ans = runOnce(()=>{
+//     console.log('hello world');
+// })
 
+
+
+// let count = 0
+// for(let i=1; i<=10; i++){
+//     count += i;
+// }
+// console.log(count)
