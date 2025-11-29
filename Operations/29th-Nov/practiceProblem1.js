cd05c2f7458fa5bb524337dce44a1e9b0a3f60f0
dev nayak10 <devprasannanayak@gmail.com>
2025-11-29 23:08:49 +0530

    content added

--- Operations/29th-Nov/practiceProblem1.js
+++ Operations/29th-Nov/practiceProblem1.js
@@ -1,12 +1,17 @@
-let arr = [20,40,10,30,50];
-let check = 0;
-for(let i = 0; i<arr.length; i++){
-    for(let j = 0; j<arr.length-1; j++){
-        if(arr[j] > arr[j+1]){
-            let temp = arr[j];
-            arr[j] = arr[j+1];
-            arr[j+1] = temp;
-        }
-    }
-}
-console.log(arr)
+// let arr = [20,40,10,30,50];
+// let check = 0;
+// for(let i = 0; i<arr.length; i++){
+//     for(let j = 0; j<arr.length-1; j++){
+//         if(arr[j] > arr[j+1]){
+//             let temp = arr[j];
+//             arr[j] = arr[j+1];
+//             arr[j+1] = temp;
+//         }
+//     }
+// }
+// console.log(arr)
+
+
+// let arr = [12,12,11,13,14];
+// console.log([...new Set(arr)].sort((a, b)=>a-b))
+
