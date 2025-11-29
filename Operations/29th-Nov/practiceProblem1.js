0ef4203266481b252c58ed180cba9c8c8689641b
dev nayak10 <devprasannanayak@gmail.com>
2025-11-29 23:00:11 +0530

    content added

--- Operations/29th-Nov/practiceProblem1.js
+++ Operations/29th-Nov/practiceProblem1.js
@@ -1,15 +1,12 @@
 let arr = [20,40,10,30,50];
 let check = 0;
-let temp = 0;
 for(let i = 0; i<arr.length; i++){
-    check = arr[i];
-    for(let j = i+1; j<=arr.length-1; j++){
-        if(check < arr[j]){
-            temp = check;
-            check = arr[j];
-            arr[j] = temp;
+    for(let j = 0; j<arr.length-1; j++){
+        if(arr[j] > arr[j+1]){
+            let temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
         }
-        temp = 0;
     }
 }
 console.log(arr)
