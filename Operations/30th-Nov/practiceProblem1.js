e3671c4d080250ace5c3a4c1da359fbe29d39891
dev nayak10 <devprasannanayak@gmail.com>
2025-11-30 23:44:49 +0530

    content added

---
+++ Operations/30th-Nov/practiceProblem1.js
@@ -0,0 +1,12 @@
+var createCounter = function(n) {
+    let ctr = n
+    return function() {
+        console.log(ctr++)
+    };
+};
+const counter = createCounter(-2)
+counter();
+counter();
+counter();
+counter();
+counter();
