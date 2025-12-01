dcc28a07ec92bfff063474cc64d9f33a05047100
dev nayak10 <devprasannanayak@gmail.com>
2025-12-01 22:05:54 +0530

    content added

--- Operations/1st-Dec/practiceProblem1.js
+++ Operations/1st-Dec/practiceProblem1.js
@@ -1,11 +1,51 @@
-function promiseCaller(args){
-    return new Promise((resolve, reject)=>{
-        if(args){
-            resolve('i am response');
-        } else {
-            reject('i am rejection');
-        }
-    })
+// function promiseCaller(args){
+//     return new Promise((resolve, reject)=>{
+//         if(args){
+//             resolve('i am response');
+//         } else {
+//             reject('i am rejection');
+//         }
+//     })
+// }
+
+// promiseCaller(true).then((msg)=>console.log(msg))
+
+
+
+
+
+
+
+function register(cb){
+    setTimeout(() => {
+        console.log('Welcome to register');
+        cb();
+    }, 2000);
 }
 
-promiseCaller(true).then((msg)=>console.log(msg))
+function welcome(cb){
+    setTimeout(() => {
+        console.log('Welcome to register');
+        cb();
+    }, 2000);
+}
+
+function login(cb){
+    setTimeout(() => {
+        console.log('Welcome to Login');
+        cb();
+    }, 2000);
+}
+
+function fetchData(cb){
+    setTimeout(() => {
+        console.log('Welcome to fetchData');
+        cb();
+    }, 2000);
+}
+
+function displayData(){
+    setTimeout(() => {
+        console.log('Welcome to displayData');
+    }, 2000);
+}
