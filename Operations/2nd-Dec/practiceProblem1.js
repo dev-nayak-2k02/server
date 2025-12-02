32224ad1f3feb873c5242ee78fff3928dc94b56e
dev nayak10 <devprasannanayak@gmail.com>
2025-12-02 22:47:06 +0530

    content added

--- Operations/2nd-Dec/practiceProblem1.js
+++ Operations/2nd-Dec/practiceProblem1.js
@@ -1,20 +1,52 @@
-let complete = function(arg){
+// let complete = function(arg){
+//     return new Promise((resolve, reject) => {
+//         if(arg){
+//             resolve('I am fullfilled');
+//         } else {
+//             reject('I am rejected');
+//         }
+//     })
+// }
+
+// function onFulFillment(res){
+//     console.log(res)
+// }
+
+// function onRejection(err) {
+//     console.log(err);
+// }
+
+// complete(true).then(onFulFillment);
+// complete(false).catch(onRejection);
+
+
+function register(){
     return new Promise((resolve, reject) => {
-        if(arg){
-            resolve('I am fullfilled');
-        } else {
-            reject('I am rejected');
-        }
+        setTimeout(() => {
+            console.log('Register Page');
+            resolve();
+        }, 2000);
     })
 }
-
-function onFulFillment(res){
-    console.log(res)
+function welcome(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('welcome Page');
+            resolve();
+        }, 2000);
+    })
 }
-
-function onRejection(err) {
-    console.log(err);
+function login(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('login Page');
+            resolve();
+        }, 2000);
+    })
 }
-
-complete(true).then(onFulFillment);
-complete(false).catch(onRejection);
+async function execute() {
+    await register();
+    await welcome();
+    await login();
+}
+execute()
