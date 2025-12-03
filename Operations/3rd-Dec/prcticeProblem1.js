b953f2ea1b606ac946b89a9d7b9063a6012545e7
dev nayak10 <devprasannanayak@gmail.com>
2025-12-04 00:33:45 +0530

    content added

--- Operations/3rd-Dec/prcticeProblem1.js
+++ Operations/3rd-Dec/prcticeProblem1.js
@@ -1,14 +1,33 @@
-let arr = [10,20,10,20,30,40,50,30,30,30];
-let obj = {};
-arr.forEach((item)=>{
-    obj[item] === undefined ? obj[item] = 1 : obj[item]++;
-})
-let count = 0;
-let k = '';
-for(let i in obj){
-    if(obj[i] > count){
-        count = obj[i];
-        k = i;
+// let arr = [10,20,10,20,30,40,50,30,30,30];
+// let obj = {};
+// arr.forEach((item)=>{
+//     obj[item] === undefined ? obj[item] = 1 : obj[item]++;
+// })
+// let count = 0;
+// let k = '';
+// for(let i in obj){
+//     if(obj[i] > count){
+//         count = obj[i];
+//         k = i;
+//     }
+// }
+// console.log(k)
+
+
+
+
+
+let executeOnce = (cb)=>{
+    let execute = false;
+    return function () {
+        if(!execute){
+            execute = true;
+            cb()
+        }
     }
 }
-console.log(k)
+let ans = executeOnce(()=>{console.log('executed once')})
+ans()
+ans()
+ans()
+ans()
