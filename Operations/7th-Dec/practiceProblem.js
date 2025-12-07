d3fe2566dec98ba361c5f2c450da10e5d0642e5b
dev nayak10 <devprasannanayak@gmail.com>
2025-12-07 23:33:40 +0530

    content added

--- Operations/7th-Dec/practiceProblem.js
+++ Operations/7th-Dec/practiceProblem.js
@@ -1,10 +1,18 @@
-let evenCheck = function(a){
-    if(a%2 == 0){
-        return 'true';
-    }
-    else {
-        return 'false';
-    }
+// let evenCheck = function(a){
+//     if(a%2 == 0){
+//         return 'true';
+//     }
+//     else {
+//         return 'false';
+//     }
+// }
+// let ans = evenCheck(13);
+// console.log(ans);
+
+
+
+let checker = function(a) {
+    a%5 == 0 ? console.log('true') : console.log('false');
 }
-let ans = evenCheck(13);
+let ans = checker(4);
 console.log(ans);
