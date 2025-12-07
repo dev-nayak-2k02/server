7ca4cce706483b630ee8598e6283816713715d1a
dev nayak10 <devprasannanayak@gmail.com>
2025-12-07 23:34:57 +0530

    content added

--- Operations/4th-dec/practiceProblem.js
+++ Operations/4th-dec/practiceProblem.js
@@ -1,13 +1,15 @@
-function timerFunction(fn, delay){
-    let call = Date.now();
-    return function (){
-        let current = Date.now();
-        if(current - call >= delay){
-            call = current;
-            fn();
-        }
-    }
-}
-let ans = timerFunction(()=>{
-    console.log('hi it happened')
-}, 2000)
+// function timerFunction(fn, delay){
+//     let call = Date.now();
+//     return function (){
+//         let current = Date.now();
+//         if(current - call >= delay){
+//             call = current;
+//             fn();
+//         }
+//     }
+// }
+// let ans = timerFunction(()=>{
+//     console.log('hi it happened')
+// }, 2000)
+
+
