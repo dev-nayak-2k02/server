33672c4459ba280d41bcf5929b00c629be5480e4
dev nayak10 <devprasannanayak@gmail.com>
2025-12-08 21:06:13 +0530

    content added

--- Operations/8th-Dec/practiceProblem.js
+++ Operations/8th-Dec/practiceProblem.js
@@ -1,12 +1,25 @@
-let calculateFactorial = (num)=>{
-    if(num <= 0) return 0;
-    let count = 0;
-    for(let i = 1; i<=num; i++){
-        if(num%i == 0){
-            count++;
-        }
+// let calculateFactorial = (num)=>{
+//     if(num <= 0) return 0;
+//     let count = 0;
+//     for(let i = 1; i<=num; i++){
+//         if(num%i == 0){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// let ans = calculateFactorial(0)
+// console.log(ans)
+
+
+
+
+let summation = a =>{
+    let sum = 0
+    for(let i = 0; i<=a; i++){
+        sum+=i;
     }
-    return count;
+    return sum;
 }
-let ans = calculateFactorial(0)
-console.log(ans)
+let ans = summation(4)
+console.log(ans);
