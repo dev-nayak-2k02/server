7d7caf5bf00741af93d97779b8813c51759f2d6e
dev nayak10 <devprasannanayak@gmail.com>
2025-12-08 21:35:13 +0530

    content added

--- Operations/8th-Dec/practiceProblem.js
+++ Operations/8th-Dec/practiceProblem.js
@@ -14,12 +14,53 @@
 
 
 
-let summation = a =>{
-    let sum = 0
-    for(let i = 0; i<=a; i++){
-        sum+=i;
+// let summation = a =>{
+//     let sum = 0
+//     for(let i = 0; i<=a; i++){
+//         sum+=i;
+//     }
+//     return sum;
+// }
+// let ans = summation(4)
+// console.log(ans);
+
+
+// let checkStringLength = function(str){
+//     let count = 0;
+//     for(let k of str){
+//         count++;
+//     }
+//     return count;
+// }
+// let result = checkStringLength('Apple');
+// console.log(result)
+
+
+// let checkVowels = str => {
+//     let count = 0;
+//     for(let k of str){
+//         if( k === 'A'||k ==='I'||k ==='E'||k ==='O'||k ==='U'||k ==='a'||k ==='i'||k ==='e'||k ==='o'||k ==='u'){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// let ans = checkVowels('Apple')
+// console.log(ans);
+
+
+
+
+let removeVowels = str => {
+    let copy = str
+    let count = 0;
+    for(let k of copy){
+        if( k === 'A'||k ==='I'||k ==='E'||k ==='O'||k ==='U'||k ==='a'||k ==='i'||k ==='e'||k ==='o'||k ==='u'){
+            copy[count] = '';
+        }
+        count++;
     }
-    return sum;
+    return copy;
 }
-let ans = summation(4)
+let ans = removeVowels('Apple')
 console.log(ans);
