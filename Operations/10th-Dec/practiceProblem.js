25611480d4aa175ebc021f123669ae261ba8db3b
dev nayak10 <devprasannanayak@gmail.com>
2025-12-10 19:39:57 +0530

    content added

--- Operations/10th-Dec/practiceProblem.js
+++ Operations/10th-Dec/practiceProblem.js
@@ -1,6 +1,9 @@
 let checkPerfecttSquare = num =>{
-    let ctr = Math.sqrt(num)
-    return ctr;
+    let ctr = Math.floor(Math.sqrt(num))
+    if(ctr*ctr === num){
+        return true
+    }
+    return false
 }
 let result = checkPerfecttSquare(56);
 console.log(result);
