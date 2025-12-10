aa2456a46a0757d3fc7c7d2fff61affe75efd743
dev nayak10 <devprasannanayak@gmail.com>
2025-12-10 20:15:11 +0530

    content added

--- Operations/10th-Dec/practiceProblem.js
+++ Operations/10th-Dec/practiceProblem.js
@@ -1,9 +1,30 @@
-let checkPerfecttSquare = num =>{
-    let ctr = Math.floor(Math.sqrt(num))
-    if(ctr*ctr === num){
-        return true
-    }
-    return false
-}
-let result = checkPerfecttSquare(56);
-console.log(result);
+// let checkPerfecttSquare = num =>{
+//     let ctr = Math.floor(Math.sqrt(num))
+//     if(ctr*ctr === num){
+//         return true
+//     }
+//     return false
+// }
+// let result = checkPerfecttSquare(56);
+// console.log(result);
+
+
+
+
+
+// let checkStringIsPalindrome = (str) => {
+//   let copyString = [...str].reverse().sort((a, b) => {
+//     return b - a;
+//   });
+//   let ltr = ''
+//   for (let k of copyString) {
+//     ltr += k
+//   }
+//   if(ltr === str){
+//     return true
+//   } else {
+//     return false
+//   }
+// };
+// let answer = checkStringIsPalindrome("NOW");
+// console.log(answer);
