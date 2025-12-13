81e7f0d926aae1c61f2574f4794b8107a34622ba
dev nayak10 <devprasannanayak@gmail.com>
2025-12-13 19:31:16 +0530

    content added

--- Operations/13th-Dec/script.js
+++ Operations/13th-Dec/script.js
@@ -1,10 +1,10 @@
-function completion (args){
-    new Promise((resolve, reject) => {
+let completion = (args)=>{
+    return new Promise((resolve, reject) => {
         const apiResponse = {
             "name": "DevPrasanna",
             "designation": "Currently works in GeeCoding"
         }
-        const statusCode = 200
+        const statusCode = 200;
         if(statusCode == 200){
             resolve(apiResponse)
         } else {
@@ -12,4 +12,6 @@ function completion (args){
         }
     })
 }
-completion(true)
+let ans = completion(true).then(result=>{
+    console.log(result.name)
+})
