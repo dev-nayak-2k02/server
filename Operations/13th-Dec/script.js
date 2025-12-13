82d898e842644caf613cf0c5314cab420d1ccbfc
dev nayak10 <devprasannanayak@gmail.com>
2025-12-13 20:38:23 +0530

    content added

--- Operations/13th-Dec/script.js
+++ Operations/13th-Dec/script.js
@@ -1,23 +1,31 @@
-let completion = (args)=>{
-    return new Promise((resolve, reject) => {
-        const apiResponse = {
-            "name": "DevPrasanna",
-            "designation": "Currently works in GeeCoding"
-        }
-        const statusCode = 600;
-        if(statusCode == 200){
-            resolve(apiResponse)
-        } else {
-            reject('ERROR_CODE')
-        }
-    })
-}
-let ans = completion(true).then(result=>{
-    for(let k in result){
-        console.log(`${k}: ${result[k]}`)
-    }
-}).catch(err=>{
-    console.log(err)
+// let completion = (args)=>{
+//     return new Promise((resolve, reject) => {
+//         const apiResponse = {
+//             "name": "DevPrasanna",
+//             "designation": "Currently works in GeeCoding"
+//         }
+//         const statusCode = 600;
+//         if(statusCode == 200){
+//             resolve(apiResponse)
+//         } else {
+//             reject('ERROR_CODE')
+//         }
+//     })
+// }
+// let ans = completion(true).then(result=>{
+//     for(let k in result){
+//         console.log(`${k}: ${result[k]}`)
+//     }
+// }).catch(err=>{
+//     console.log(err)
+// })
+
+
+
+
+let arr = [11,12,11,11,11,13,14];
+let obj = {};
+arr.forEach((val)=>{
+    (obj[val] === undefined) ? (obj[val] = 1) : (obj[val]++)
 })
-
-
+console.log(obj)
