968db2ea8e91433769219f2d095f60bbdf60a908
dev nayak10 <devprasannanayak@gmail.com>
2025-12-15 23:13:00 +0530

    content added

---
+++ Operations/15th-Dec/script.js
@@ -0,0 +1,13 @@
+// let calculateFactorial = num => {
+//     let count = 0;
+//     for(let i = 1; i<=num; i++){
+//         if(num%i == 0){
+//             count++;
+//         }
+//     }
+//     return count
+// }
+// let result = calculateFactorial(12)
+// console.log(result)
+
+/
