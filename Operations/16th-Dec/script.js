32ebedfd656c5e594edf7d5d0ff54acfb7202c37
dev nayak10 <devprasannanayak@gmail.com>
2025-12-16 21:26:27 +0530

    content added

--- Operations/16th-Dec/script.js
+++ Operations/16th-Dec/script.js
@@ -6,16 +6,32 @@
 //     return 'works'
 // }
 
-var ans = (function testing() {
-    let val = 10;
-    return {
-        set: function (num){
-            val = num;
-        },
-        get: function (){
-            return val
-        }
+// var ans = (function testing() {
+//     let val = 10;
+//     return {
+//         set: function (num){
+//             val = num;
+//         },
+//         get: function (){
+//             return val
+//         }
+//     }
+// })();
+// console.log(ans.get())
+// ans.set(12)
+
+
+// let obj = new Object();
+// obj = {
+//     num: 13
+// }
+// console.log(obj)
+
+
+for(let i=1; i<=4; i++){
+    let star = '';
+    for(let j=1; j<=i; j++){
+        star += '*';
     }
-})();
-console.log(ans.get())
-ans.set(12)
+    console.log(star);
+}
