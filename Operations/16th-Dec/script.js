224f4265187554ae6256a1501a7cbf6a1bd93aa6
dev nayak10 <devprasannanayak@gmail.com>
2025-12-16 21:03:49 +0530

    content added

--- Operations/16th-Dec/script.js
+++ Operations/16th-Dec/script.js
@@ -1,7 +1,21 @@
 // let str = 'Apple';
 // console.log(str.slice(2,5))
 
-console.log(myFunc());
-function myFunc(){
-    return 'works'
-}
+// console.log(myFunc());
+// function myFunc(){
+//     return 'works'
+// }
+
+var ans = (function testing() {
+    let val = 10;
+    return {
+        set: function (num){
+            val = num;
+        },
+        get: function (){
+            return val
+        }
+    }
+})();
+console.log(ans.get())
+ans.set(12)
