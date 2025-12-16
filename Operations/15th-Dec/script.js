15c1bebdbaac0206748afc64dcc6389b5aca0b14
dev nayak10 <devprasannanayak@gmail.com>
2025-12-16 08:34:08 +0530

    content added

--- Operations/15th-Dec/script.js
+++ Operations/15th-Dec/script.js
@@ -10,4 +10,18 @@
 // let result = calculateFactorial(12)
 // console.log(result)
 
-/
+let functionRedeemer = (fn, delay) => {
+    let call = 0;
+    return function () {
+        let current = Date.now();
+        if(current - call >= delay){
+            call = current;
+            fn();
+        }
+    }
+
+}
+let answer = functionRedeemer(()=>{
+    console.log('I am executing');
+}, 2000/*in ms*/);
+answer()
