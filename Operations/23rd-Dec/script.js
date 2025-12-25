3a37c19397dc5d02d81008be6476868bf4395378
dev nayak10 <devprasannanayak@gmail.com>
2025-12-25 20:01:34 +0530

    content added

--- Operations/23rd-Dec/script.js
+++ Operations/23rd-Dec/script.js
@@ -3,6 +3,6 @@
 // console.log(newArr)
 
 
-let arr = [1,1,2,3,4,1,3,6,7,7]
-let newArr = [...new Set(arr)].sort((a,b) => {return b - a});
-console.log(newArr[1])
+// let arr = [1,1,2,3,4,1,3,6,7,7]
+// let newArr = [...new Set(arr)].sort((a,b) => {return b - a});
+// console.log(newArr[1])
