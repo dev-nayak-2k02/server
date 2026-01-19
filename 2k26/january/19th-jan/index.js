43bd76ec966a9a6176551f2a1bf0b356333ac10a
dev nayak10 <devprasannanayak@gmail.com>
2026-01-19 12:47:23 +0530

    content added

--- 2k26/january/19th-jan/index.js
+++ 2k26/january/19th-jan/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 dotenv.config()
+connectDB()
 const app = express()
 const PORT = process.env.PORT || 3000;
 
