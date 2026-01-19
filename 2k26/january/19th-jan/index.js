c89e8e9940090fa13c25017c2bb11269406cc8cc
dev nayak10 <devprasannanayak@gmail.com>
2026-01-19 13:11:05 +0530

    content added

--- 2k26/january/19th-jan/index.js
+++ 2k26/january/19th-jan/index.js
@@ -4,6 +4,7 @@ const connectDB = require('./config/db')
 dotenv.config()
 connectDB()
 const app = express()
+app.use("/api/v1", require('./routes/userRoutes'))
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=>{
--- 2k26/january/19th-jan/routes/userRoutes.js
+++ 2k26/january/19th-jan/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { loginController, registerController } = require('../controller/userController')
 const router = express.Router()
+
 router.get('/login', loginController)
 router.get('/register', registerController)
 
