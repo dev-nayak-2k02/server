48461c8ea00796d1a4dd6eb71f5a0c7ba9cd63af
dev nayak10 <devprasannanayak@gmail.com>
2026-01-22 23:17:27 +0530

    content added

--- 2k26/january/20th-jan/index.js
+++ 2k26/january/20th-jan/index.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv')
 const app = express()
 dotenv.config()
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, ()=>{
-    console.log(`server running at PORT: ${PORT}`);
+    console.log(`server running at PORT.No: ${PORT}`);
 })
