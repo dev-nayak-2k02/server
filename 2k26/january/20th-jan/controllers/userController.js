9e2a294bdf0b0502caee2ae857ba7851b3c3474c
dev nayak10 <devprasannanayak@gmail.com>
2026-02-01 22:33:51 +0530

    content added

--- 2k26/january/20th-jan/controllers/userController.js
+++ 2k26/january/20th-jan/controllers/userController.js
@@ -1,3 +1,8 @@
 const registerController = (req, res)=>{
     res.send(`registraion page`)
 }
+const loginController = (req, res)=>{
+    res.send(`login controller`)
+}
+
+module.exports = {registerController, loginController}
