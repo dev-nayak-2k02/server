cf307008d23d048221cbc66b2767c76d707a2fb7
dev nayak10 <devprasannanayak@gmail.com>
2026-02-09 22:14:34 +0530

    content added

--- 2k26/february/9th-feb/index.js
+++ 2k26/february/9th-feb/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.get('/test', (req,res)=> {
     res.status(200).send('welcome to node server')
 })
-const PORT = process.env.PORT || 3060
+const PORT = process.env.PORT || 3060;
 app.listen(PORT, ()=>{
     console.log(`PORT:${PORT}, ${process.env.DEV_NODE}`);
 })
