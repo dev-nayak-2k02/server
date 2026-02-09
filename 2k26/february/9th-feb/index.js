ba2c558825ece91d4c15956089c80f670e48d469
dev nayak10 <devprasannanayak@gmail.com>
2026-02-09 21:55:36 +0530

    content added

--- 2k26/february/9th-feb/index.js
+++ 2k26/february/9th-feb/index.js
@@ -1,2 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+dotenv.config()
+connectDB()
+const app = express()
+app.get('/test', (req,res)=> {
+    res.status(200).send('welcome to node server')
+})
+const PORT = process.env.PORT || 3060
+app.listen(PORT, ()=>{
+    console.log(`PORT:${PORT}, ${process.env.DEV_NODE}`);
+})
