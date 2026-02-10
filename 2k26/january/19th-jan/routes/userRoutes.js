ff4f6606efae6aa92e53532bbdbb4d349a404c6e
dev nayak10 <devprasannanayak@gmail.com>
2026-02-10 21:43:00 +0530

    content added

--- 2k26/january/19th-jan/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const { loginController, registerController } = require('../controller/userController')
-const router = express.Router()
-
-router.get('/login', loginController)
-router.get('/register', registerController)
-
-module.exports = router
