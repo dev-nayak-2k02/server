dfae5e8eca49aa8acfc7c16431ed7435d526ff75
dev nayak10 <devprasannanayak@gmail.com>
2026-02-10 21:47:44 +0530

    content added

--- 2k26/february/9th-feb/model/userModel.js
+++ 2k26/february/9th-feb/model/userModel.js
@@ -1,1 +1,21 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps },
+);
+const userModel = mongoose.model("user", userSchema);
+module.exports = userModel;
