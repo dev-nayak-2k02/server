588a6546aab86c604b190a54f9d965f123474fa2
dev nayak10 <devprasannanayak@gmail.com>
2026-03-04 09:59:21 +0530

    content added

--- Operations/4th-March/index.js
+++ Operations/4th-March/index.js
@@ -1,6 +1,14 @@
 
-function adder(a, b){
-    return a+b;
-}
-let sum = adder(1, 2);
-console.log(sum);
+// function adder(a, b){
+//     return a+b;
+// }
+// let sum = adder(1, 2);
+// console.log(sum);
+
+
+
+let arr = [1, 2, 3, 4, 5];
+let newArr = arr.map((num)=>{
+    return num;
+})
+console.log(newArr);
