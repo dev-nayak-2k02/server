66b57c1a825a84b07538fc59c54b9a673358706f
dev nayak10 <devprasannanayak@gmail.com>
2026-05-12 08:50:54 +0530

    content added

---
+++ Operations/12th-may/script.js
@@ -0,0 +1,7 @@
+let a = Number(prompt("Enter 1st number: "));
+let b = Number(prompt("Enter 2nd number: "));
+let add = (a,b)=>{
+    return a+b;
+}
+let res = add(a,b);
+console.log(`sum = ${res}`);
