d242bcea2becfa948c8e0ed008e9d1c340880102
dev nayak10 <devprasannanayak@gmail.com>
2026-05-16 16:59:24 +0530

    content added

--- Operations/16th-May/Script.js
+++ Operations/16th-May/Script.js
@@ -1,3 +1,31 @@
-let a = true;
-let b = false;
-console.log(a || b);
+// let a = true;
+// let b = false;
+// console.log(a || b);
+
+// const person = {
+//     id: 1123,
+//     name: 'Dev Prasanna',
+//     age: 23,
+//     operation: 'Web-developer'
+// }
+// const { operation } = person;
+// console.log(operation);
+
+let charArray = [
+  {
+    id: 1123,
+    name: "Dev Prasanna",
+    age: 23,
+    operation: "Web-developer",
+  },
+  {
+    id: 1124,
+    name: "Pushkarashin",
+    age: 24,
+    operation: "data-Analyst",
+  }
+];
+
+
+const [e1, e2] = charArray;
+console.log(e1.name, e2.name);
