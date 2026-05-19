7bf0841a6df7826449376d6e31417e4b34ec7ac2
dev nayak10 <devprasannanayak@gmail.com>
2026-05-19 08:56:23 +0530

    content added

--- Operations/16th-May/Script.js
+++ Operations/16th-May/Script.js
@@ -33,45 +33,45 @@
 // console.log([999,...arr1,90]);
 
 const personsArray = [
-    {
-        name:'Dev Nayak',
-        age:40
-    }, {
-        name: 'Shivam',
-        age:45
-    }, {
-        name: 'shubham',
-        age:50
-    }
+  {
+    name: "Dev Nayak",
+    age: 40,
+  },
+  {
+    name: "Shivam",
+    age: 45,
+  },
+  {
+    name: "shubham",
+    age: 50,
+  },
 ];
 // const result = personsArray.filter((val, index)=>{
 //     return (val.age < 50);
 // })
 // console.log(result);
 
-const result = personsArray.findIndex((val, index)=>{
-    return val.age === 45;
-});
-console.log(result);
+// const result = personsArray.findIndex((val, index) => {
+//   return val.age === 45;
+// });
+// console.log(result);
 
-
-
-
-
-
-
-let descriptionElement = document.querySelector('.desc');
-function renderer(p){
-    descriptionElement.innerHTML = p.map(a=>`<p>${p.description}</p>`)
+let descriptionElement = document.querySelector(".desc");
+function renderer(p) {
+  descriptionElement.innerHTML = p
+    .map((a) => `<p>${a.title}</p>`)
+    .join(" ");
 }
 async function fetchList() {
-    try {
-        const apiResponse = await fetch('https://dummyjson.com/products', ()=>{
-            method:'GET'
-        })
-        let res = await apiResponse.json();
-
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const apiResponse = await fetch("https://dummyjson.com/products", {
+      method: "GET"
+    });
+    const res = await apiResponse.json();
+    console.log(res.products[0].title);
+    if (res?.products?.length > 0) renderer(res?.products);
+  } catch (error) {
+    console.log(error);
+  }
 }
+fetchList()
