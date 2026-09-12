43e596d55fb705e9635ff0f912cf81e054aefdee
dev nayak10 <devprasannanayak@gmail.com>
2026-09-12 19:08:56 +0530

    content added

--- Operations/10th-Dec/practiceProblem.js
+++ Operations/10th-Dec/practiceProblem.js
@@ -8,10 +8,6 @@
 // let result = checkPerfecttSquare(56);
 // console.log(result);
 
-
-
-
-
 // let checkStringIsPalindrome = (str) => {
 //   let copyString = [...str].reverse().sort((a, b) => {
 //     return b - a;
@@ -29,10 +25,6 @@
 // let answer = checkStringIsPalindrome("NOW");
 // console.log(answer);
 
-
-
-
-
 // let executeOnce = (fn) =>{
 //     let executed = false;
 //     return function (){
@@ -46,13 +38,8 @@
 //     console.log('I got executed once')
 // })
 
-
-
-
-
 // let arr = [1,2,3];
 // console.log(arr=[]);
-
 
 // const dummyObject = {
 //     id:12,
@@ -68,13 +55,9 @@
 // const{ id,name,jobTitle } = dummyObject;
 // console.log(id,name,jobTitle);
 
-
-
-
 // const array = ['ad','ed','eddy'];
 // const [e,a,b] = array;
 // console.log(e,a,b);
-
 
 // const array = [1,2,3,4,5,6,7];
 // let newArray = array.filter((value, index)=>{
@@ -82,15 +65,12 @@
 // });
 // console.log(newArray);
 
-
-
 // function getName(name){
 //     return name;
 // }
 // let a = false;
 // let b = false;
 // console.log(a || b);
-
 
 // let showRecipe = true;
 // function getOne(nm){
@@ -101,7 +81,6 @@
 //     return nm;
 // }
 // showRecipe ? console.log(getOne('Dev')):console.log(getTwo('Pizza'));
-
 
 // const id = 1;
 // const prodName = 'Apple Watch';
@@ -116,66 +95,56 @@
 // const {description} = product1;
 // console.log(description);
 
-
-
 // const array = [1,2,3];
 // const[a,b,c] = array;
 // console.log(b);
 
-
-
 const personsArray = [
-    {
-        name:'Dev',
-        age:50,
-        country:'USA'
-    },
-    {
-        name:'shivam',
-        age:23,
-        country:'EU'
-    },
-    {
-        name:'swaroop',
-        age:24,
-        country:'USA'
-    },
-    {
-        name:'singham',
-        age:26,
-        country:'NZ'
-    }
-]
+  {
+    name: "Dev",
+    age: 50,
+    country: "USA",
+  },
+  {
+    name: "shivam",
+    age: 23,
+    country: "EU",
+  },
+  {
+    name: "swaroop",
+    age: 24,
+    country: "USA",
+  },
+  {
+    name: "singham",
+    age: 26,
+    country: "NZ",
+  },
+];
 // let getAllItems = personsArray.find((val, index)=>{
 //     return val.name === 'swaroop';
 // })
 // console.log(getAllItems);
-
 
 // let someExperiment = personsArray.every((val,index)=>{
 //     return val.age === 26;
 // })
 // console.log(someExperiment);
 
-
 // let findIndexExperiment = personsArray.findIndex((val, index)=>{
 //     return val.age == 26;
 // })
 // console.log(findIndexExperiment);
 
-
 // let num = 100;
 // let sumOfNTerms = Math.floor(num*(num+1)/2);
 // console.log(sumOfNTerms);
-
 
 // for(let i = 0; i<51; i++){
 //     if(i%3 == 0){
 //         console.log(i)
 //     }
 // }
-
-
 
 // let user = +(prompt("Enter a number: "));
 // for(let i=1; i<user+1; i++){
@@ -186,15 +155,11 @@ const personsArray = [
 //     }
 // }
 
-
-
-
 // for(let i = 1; i<101; i++){
 //     if(i%3 == 0 && i%5 == 0){
 //         console.log(i);
 //     }
 // }
-
 
 // for(let i = 1; i<101; i++){
 //     if(i%7 == 0){
@@ -203,8 +168,19 @@ const personsArray = [
 //     console.log(i);
 // }
 
+// for(let i = 1; i<21; i++){
+//     if(i%3 == 0) continue;
+//     console.log(i);
 
-for(let i = 1; i<21; i++){
-    if(i%3 == 0) continue;    
-    console.log(i);
+// }
+
+let ctr = 0;
+for (let i = 1; i < 100; i++) {
+  if (i % 3 == 0) {
+    ctr++;
+  }
+  if(ctr == 5){
+    break;
+  }
+  console.log();
 }
